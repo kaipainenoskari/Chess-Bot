557a8916817dd66fe93eb206786bdb45a337bd14
kaipainenoskari <oskari.kaipainen@hotmail.com>
2024-10-31 13:41:06 +0200

    First commit

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -1,5 +1,6 @@
-﻿using ChessChallenge.API;
+using ChessChallenge.API;
 using System;
+using System.Linq;
 
 namespace ChessChallenge.Example
 {
@@ -7,48 +8,181 @@ namespace ChessChallenge.Example
     // Plays randomly otherwise.
     public class EvilBot : IChessBot
     {
-        // Piece values: null, pawn, knight, bishop, rook, queen, king
-        int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+        private int GetPieceValue(PieceType pieceType)
+        {
+            return pieceType switch
+            {
+                PieceType.Pawn => 100,
+                PieceType.Knight => 320,
+                PieceType.Bishop => 350,
+                PieceType.Rook => 500,
+                PieceType.Queen => 900,
+                PieceType.King => 20000,
+                _ => 0
+            };
+        }
+
+        private int GetMoveScore(Move move, Board board)
+        {
+            int score = 0;
+
+            // MVV-LVA: Most Valuable Victim - Least Valuable Attacker
+            if (move.IsCapture)
+            {
+                int victimValue = GetPieceValue(move.CapturePieceType);
+                int attackerValue = GetPieceValue(move.MovePieceType);
+                score += (victimValue * 10) - attackerValue;
+            }
+
+            // Promotions
+            if (move.IsPromotion)
+            {
+                int promotionValue = GetPieceValue(move.PromotionPieceType);
+                score += promotionValue * 10;
+            }
+
+            // Checks
+            board.MakeMove(move);
+            if (board.IsInCheck())
+            {
+                score += 10000; // Arbitrary high value to prioritize checks
+            }
+            board.UndoMove(move);
+
+            return score;
+        }
+
+        private Move[] OrderMoves(Move[] moves, Board board)
+        {
+            //return moves;
+            return moves.OrderByDescending(move => GetMoveScore(move, board)).ToArray();
+        }
+
+        private static int GetScore(Board board)
+        {
+            if (board.IsInCheckmate())
+            {
+                return board.IsWhiteToMove ? int.MinValue : int.MaxValue;
+            }
+            if (board.IsInStalemate())
+            {
+                return 0;
+            }
+            int score = 0;
+            PieceList[] allPieces = board.GetAllPieceLists();
+
+            for (int i = 0; i < allPieces.Length; i++)
+            {
+                int multiplier = (i < 6) ? 1 : -1;
+                foreach(Piece piece in allPieces[i])
+                {
+                    score += multiplier * piece.PieceType switch
+                    {
+                        PieceType.Pawn => 100,
+                        PieceType.Knight => 320,
+                        PieceType.Bishop => 330,
+                        PieceType.Rook => 500,
+                        PieceType.Queen => 900,
+                        PieceType.King => 20000,
+                        _ => 0
+                    };
+                }
+            }
+            return score;
+        }
+
+        private (Move, int) AlphaBeta(Board board, int depth, bool isMaximizingPlayer, int alpha = int.MinValue, int beta = int.MaxValue)
+        {
+            // Base case: if depth is 0, evaluate and return the score of the current board
+            if (depth == 0)
+            {
+                return (board.GetLegalMoves().FirstOrDefault(), GetScore(board));
+            }
+
+            Move[] moves = OrderMoves(board.GetLegalMoves(), board);
+
+            int bestScore = isMaximizingPlayer ? int.MinValue : int.MaxValue;
+            Move bestMove = moves.FirstOrDefault();
+
+            foreach (Move move in moves)
+            {
+                board.MakeMove(move);
+                int score;
+
+                if (board.IsInCheckmate())
+                {
+                    score = isMaximizingPlayer ? int.MaxValue : int.MinValue;
+                }
+                else if (board.IsInStalemate())
+                {
+                    score = 0;
+                }
+                else
+                {
+                    // Recursively call MinMax with alpha-beta pruning, decreasing depth
+                    score = AlphaBeta(board, depth - 1, !isMaximizingPlayer, alpha, beta).Item2;
+                }
+
+                // Update the best score and move based on maximizing or minimizing player
+                if (isMaximizingPlayer)
+                {
+                    if (score > bestScore)
+                    {
+                        bestScore = score;
+                        bestMove = move;
+                    }
+                    alpha = Math.Max(alpha, bestScore);
+
+                    // Alpha-beta pruning: cut off exploration if the score is already worse than beta
+                    if (beta <= alpha)
+                    {
+                        board.UndoMove(move);
+                        break; // Prune the remaining branches
+                    }
+                }
+                else
+                {
+                    if (score < bestScore)
+                    {
+                        bestScore = score;
+                        bestMove = move;
+                    }
+                    beta = Math.Min(beta, bestScore);
+
+                    // Alpha-beta pruning: cut off exploration if the score is already worse than alpha
+                    if (beta <= alpha)
+                    {
+                        board.UndoMove(move);
+                        break; // Prune the remaining branches
+                    }
+                }
+
+                board.UndoMove(move);
+            }
+
+            return (bestMove, bestScore);
+        }
 
         public Move Think(Board board, Timer timer)
         {
-            Move[] allMoves = board.GetLegalMoves();
-
-            // Pick a random move to play if nothing better is found
-            Random rng = new();
-            Move moveToPlay = allMoves[rng.Next(allMoves.Length)];
-            int highestValueCapture = 0;
-
-            foreach (Move move in allMoves)
+            int maxDepth = 5;
+            double timeLimit = timer.MillisecondsRemaining * 0.1 / 20.0;
+            Move bestMove = board.GetLegalMoves().FirstOrDefault();
+            int i = 1;
+            DateTime startTime = DateTime.Now;
+            while (i <= maxDepth)
             {
-                // Always play checkmate in one
-                if (MoveIsCheckmate(board, move))
+                (Move move, int score) = AlphaBeta(board, i, board.IsWhiteToMove);
+                bestMove = move;
+                double elapsedTime = (DateTime.Now - startTime).TotalMilliseconds / 1000.0;
+                //Console.WriteLine($"Depth: {i}, Time: {Math.Round(elapsedTime, 2, MidpointRounding.AwayFromZero)}s");
+                if (elapsedTime > timeLimit)
                 {
-                    moveToPlay = move;
                     break;
                 }
-
-                // Find highest value capture
-                Piece capturedPiece = board.GetPiece(move.TargetSquare);
-                int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
-
-                if (capturedPieceValue > highestValueCapture)
-                {
-                    moveToPlay = move;
-                    highestValueCapture = capturedPieceValue;
-                }
+                i++;
             }
-
-            return moveToPlay;
-        }
-
-        // Test if this move gives checkmate
-        bool MoveIsCheckmate(Board board, Move move)
-        {
-            board.MakeMove(move);
-            bool isMate = board.IsInCheckmate();
-            board.UndoMove(move);
-            return isMate;
+            return bestMove;
         }
     }
 }
--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,307 @@
-﻿using ChessChallenge.API;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
+    bool isWhite;
+    Move moveToPlay;
+    int depth = 5;
+    Board boardRef;
+    Timer timerRef;
+    int maxTime;
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        boardRef = board;
+        timerRef = timer;
+        isWhite = boardRef.IsWhiteToMove;
+        // As there are less pieces search to higher depths
+        int pieceCount = 0;
+        Array.ForEach(boardRef.GetAllPieceLists(), list => pieceCount += list.Count);
+        depth = pieceCount < 5 ? 7 : pieceCount < 10 ? 5 : 3;
+
+        maxTime = timerRef.MillisecondsRemaining / 20;
+        if (timerRef.MillisecondsRemaining < 5000)
+        {
+            maxTime = 500;
+            depth = 3;
+        }
+        depth = 3;
+
+        int evaluation = NegaMax(depth, isWhite ? 1 : -1);
+        Console.WriteLine("Evaluation: " + evaluation);
+        return moveToPlay;
     }
+    private int NegaMax(int currentDepth, int color, int alpha = -600000, int beta = 600000)
+    {
+        if (timerRef.MillisecondsElapsedThisTurn > maxTime)
+            return 500000;
+        if (boardRef.IsInCheckmate() || boardRef.IsDraw())
+            return Evaluate(color, currentDepth);
+
+        // If depth is 0, perform quiescence search
+        if (currentDepth == 0)
+            return QuiescenceSearch(currentDepth, color, alpha, beta);
+
+        Move[] moves = OrderMoves();
+        foreach (Move move in moves)
+        {
+            boardRef.MakeMove(move);
+            int score = -NegaMax(currentDepth - 1, -color, -beta, -alpha);
+            boardRef.UndoMove(move);
+
+            if (score >= beta)
+                return beta;
+            if (score > alpha)
+            {
+                alpha = score;
+                if (currentDepth == depth)
+                    moveToPlay = move;
+            }
+        }
+        return alpha;
+    }
+    int QuiescenceSearch(int currentDepth, int color, int alpha, int beta)
+    {
+        int standPat = Evaluate(color, currentDepth);
+        if (standPat >= beta)
+            return beta;
+        if (alpha < standPat)
+            alpha = standPat;
+
+        /*
+        List<Move> moveList = OrderMoves(capturesOnly: true).ToList();
+        Move[] allMoves = boardRef.GetLegalMoves();
+        foreach (Move move in allMoves)
+        {
+            boardRef.MakeMove(move);
+            if (boardRef.IsInCheck())
+                moveList.Insert(0, move);
+            boardRef.UndoMove(move);
+        }
+        Move[] moves = moveList.ToArray();
+        */
+        Move[] moves = OrderMoves(capturesOnly: true);
+        foreach (Move move in moves)
+        {
+            boardRef.MakeMove(move);
+            int score = -QuiescenceSearch(currentDepth - 1, -color, -beta, -alpha);
+            boardRef.UndoMove(move);
+
+            if (score >= beta)
+                return beta;
+            if (alpha < score)
+                alpha = score;
+        }
+        return alpha;
+    }
+    private int Evaluate(int color, int currentDepth)
+    {
+        if (boardRef.IsInCheckmate())
+            return -500000 - currentDepth; // Prioritize quicker checkmates
+        if (boardRef.IsDraw())
+            return 100; // Arbitrary value to discourage draws
+        int score = 0;
+        PieceList[] allPieceLists = boardRef.GetAllPieceLists();
+
+        foreach (PieceList pieceList in allPieceLists)
+        {
+            int pieceValue = GetPieceValue(pieceList.TypeOfPieceInList);
+            foreach(Piece piece in pieceList)
+            {
+                int pieceSquareValue = GetPieceSquareValue(piece) + pieceValue;
+                score += (piece.IsWhite ? 1 : -1) * pieceSquareValue;
+            }
+        }
+        int attackBonus = (boardRef.IsWhiteToMove ? 1 : -1) * boardRef.GetLegalMoves(capturesOnly: true).Length * 30;
+        score += attackBonus;
+
+        return score * color;
+    }
+    private static int GetPieceValue(PieceType pieceType)
+    {
+        return pieceType switch
+        {
+            PieceType.Pawn => 100,
+            PieceType.Knight => 320,
+            PieceType.Bishop => 350,
+            PieceType.Rook => 500,
+            PieceType.Queen => 900,
+            PieceType.King => 20000,
+            _ => 0
+        };
+    }
+    private bool IsEndGame()
+    {
+        int totalMaterial = 0;
+        PieceList[] allPieces = boardRef.GetAllPieceLists();
+
+        foreach (var pieceList in allPieces)
+        {
+            foreach (Piece piece in pieceList)
+            {
+                // Exclude kings and pawns from material count
+                if (piece.PieceType != PieceType.King && piece.PieceType != PieceType.Pawn)
+                    totalMaterial += GetPieceValue(piece.PieceType);
+            }
+        }
+        // If total material (excluding kings and pawns) is below or equal to 1300 centipawns
+        return totalMaterial <= 1300;
+    }
+    private int GetMoveScore(Move move)
+    {
+        int score = 0;
+
+        // MVV-LVA: Most Valuable Victim - Least Valuable Attacker
+        if (move.IsCapture)
+        {
+            int victimValue = GetPieceValue(move.CapturePieceType);
+            int attackerValue = GetPieceValue(move.MovePieceType);
+            score += 10 * (victimValue - attackerValue);
+        }
+
+        // Promotions
+        if (move.IsPromotion)
+        {
+            int promotionValue = GetPieceValue(move.PromotionPieceType) - GetPieceValue(move.MovePieceType);
+            score += promotionValue;
+        }
+
+        /*
+        // Checks
+        boardRef.MakeMove(move);
+        if (boardRef.IsInCheck())
+        {
+            score += 5000; // Arbitrary high value to prioritize checks
+            if (boardRef.IsInCheckmate())
+            {
+                score += 100000; // Arbitrary high value to prioritize checkmates
+            }
+        }
+        boardRef.UndoMove(move);
+        */
+
+        return score;
+    }
+    private Move[] OrderMoves(bool capturesOnly = false)
+    {
+        Move[] moves = boardRef.GetLegalMoves(capturesOnly: capturesOnly);
+        return moves.OrderByDescending(move => GetMoveScore(move)).ToArray();
+    }
+    private int GetPieceSquareValue(Piece piece)
+    {
+        int file = piece.Square.File;
+        int rank = piece.Square.Rank;
+
+        // Mirror the file if it's on the right half
+        if (file > 3)
+            file = 7 - file;
+
+        // Invert rank for black pieces
+        if (!piece.IsWhite)
+            rank = 7 - rank;
+        bool isEndGame = IsEndGame();
+
+        return piece.PieceType switch
+        {
+            PieceType.Pawn => isEndGame ? HalfFilePawnEndgameTable[rank, file] : HalfFilePawnMiddleTable[rank, file],
+            PieceType.Knight => HalfFileKnightTable[rank, file],
+            PieceType.Bishop => HalfFileBishopTable[rank, file],
+            PieceType.Rook => HalfFileRookTable[rank, file],
+            PieceType.Queen => HalfFileQueenTable[rank, file],
+            PieceType.King => IsEndGame() ? HalfFileKingEndgameTable[rank, file] : HalfFileKingMiddleTable[rank, file],
+            _ => 0,
+        };
+    }
+    private static readonly int[,] HalfFilePawnMiddleTable = new int[8, 4]
+    {
+        { 0,   0,   0,   0 },
+        { 5,  10,  10, -20 },
+        { 5,  -5, -10,   0 },
+        { 0,   0,   0,  20 },
+        { 5,   5,  10,  25 },
+        { 10, 10,  20,  30 },
+        { 50, 50,  50,  50 },
+        { 0,   0,   0,   0 }
+    };
+    private static readonly int[,] HalfFilePawnEndgameTable = new int[8, 4]
+    {
+        { 0,   0,   0,   0   },
+        { 15,  5,   5,  10   },
+        { 5,   5,  -5,   0   },
+        { 10,  10, -5,  -5   },
+        { 30,  25,  10,  5   },
+        { 90,  100, 85,  65  },
+        { 180, 170, 160, 130 },
+        { 0,   0,   0,   0   }
+    };
+    private static readonly int[,] HalfFileKnightTable = new int[8, 4]
+    {
+        { -50, -40, -30, -30 },
+        { -40, -20,   0,   5 },
+        { -30,   5,  10,  15 },
+        { -30,   0,  15,  20 },
+        { -30,   5,  15,  20 },
+        { -30,   0,  10,  15 },
+        { -40, -20,   0,   0 },
+        { -50, -40, -30, -30 }
+    };
+    private static readonly int[,] HalfFileBishopTable = new int[8, 4]
+    {
+        { -20, -10, -10, -10 },
+        { -10,   5,   0,   0 },
+        { -10,  10,  10,  10 },
+        { -10,   0,  10,  10 },
+        { -10,   5,   5,  10 },
+        { -10,   0,   5,  10 },
+        { -10,   0,   0,   0 },
+        { -20, -10, -10, -10 }
+    };
+    private static readonly int[,] HalfFileRookTable = new int[8, 4]
+    {
+        {   0,   0,   0,   5 },
+        {  -5,   0,   0,   0 },
+        {  -5,   0,   0,   0 },
+        {  -5,   0,   0,   0 },
+        {  -5,   0,   0,   0 },
+        {  -5,   0,   0,   0 },
+        {   5,  10,  10,  10 },
+        {   0,   0,   0,   0 }
+    };
+    private static readonly int[,] HalfFileQueenTable = new int[8, 4]
+    {
+        { -20, -10, -10,  -5 },
+        { -10,   0,   5,   0 },
+        { -10,   5,   5,   5 },
+        {  -5,   0,   5,   5 },
+        {   0,   0,   5,   5 },
+        { -10,   0,   5,   5 },
+        { -10,   0,   0,   0 },
+        { -20, -10, -10,  -5 }
+    };
+    private static readonly int[,] HalfFileKingMiddleTable = new int[8, 4]
+    {
+        {  60,  80,  50,   0 },
+        {  20,  20,   0,   0 },
+        { -10, -20, -20, -20 },
+        { -20, -30, -30, -40 },
+        { -30, -40, -40, -50 },
+        { -30, -40, -40, -50 },
+        { -30, -40, -40, -50 },
+        { -30, -40, -40, -50 },
+    };
+    private static readonly int[,] HalfFileKingEndgameTable = new int[8, 4]
+    {
+        { -50, -40, -30, -20 },
+        { -30, -20, -10,   0 },
+        { -30, -10,  20,  30 },
+        { -30, -10,  30,  40 },
+        { -30, -10,  30,  40 },
+        { -30, -10,  20,  30 },
+        { -30, -30,   0,   0 },
+        { -50, -30, -30, -30 }
+    };
 }
